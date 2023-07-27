cdf39e71e898b3b456e239f6256a765cc7fab53a
Thang <100179094+Thangtran496@users.noreply.github.com>
2023-07-27 21:30:16 +0700

    Create repo

---
+++ src/main/java/com/example/demo/DeThiDinhApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeThiDinhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeThiDinhApplication.class, args);
+	}
+
+}
