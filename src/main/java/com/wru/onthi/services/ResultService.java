ee1eef5262c3dc99f0f8d49e838febfa1fedfe7b
quocvix <quocvixcode@gmail.com>
2023-08-07 01:40:45 +0700

    Code_remake

---
+++ src/main/java/com/wru/onthi/services/ResultService.java
@@ -0,0 +1,21 @@
+package com.wru.onthi.services;
+
+import java.security.cert.PKIXRevocationChecker.Option;
+import java.util.List;
+import java.util.Optional;
+
+import com.wru.onthi.entity.Result;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface ResultService {
+    void save(Result result);
+    Optional<Result> findById(Integer id);
+    Optional<Result> checkResultExist(Integer timeout, Integer userId, Integer ExamId,Integer status);
+    Page<Result> getListResultExam(Pageable pageable);
+    Page<Result> searchResultExam(String username, String examCode, Pageable pageable);
+    void deleteResult(Result result);
+    List<Result> getResultScoreDESC();
+    long countResult();
+    Page<Result> getListResultExamByUserId(Integer userId, Pageable pageable);
+}
---
+++ src/main/java/com/wru/onthi/utils/PasswordGenerator.java
@@ -0,0 +1,17 @@
+package com.wru.onthi.utils;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+public class PasswordGenerator {
+    public static void main(String[] args) {
+//        BCryptPasswordEncoder encoder= new BCryptPasswordEncoder();
+//        String pass= encoder.encode("12345");
+
+//        Date date= new Date();
+//        long time= date.getTime();
+//        System.out.println("time stamp"+time);
+    }
+}
